3786a8caf1dfadf50bc756e3c1c13fc69ee432f8
mgm <archwizmgm@gmail.com>
2023-10-19 17:19:32 +0800

    新建Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM debian:12
+COPY ./ /usr/clash-for-linux
+RUN sed -i 's/deb.debian.org/mirrors.ustc.edu.cn/g' /etc/apt/sources.list.d/debian.sources
+RUN apt-get update && apt-get install -y \
+    openssl \
+    curl
+CMD [ "bash","/usr/clash-for-linux/start.sh" ]
--- start.sh
+++ start.sh
@@ -9,7 +9,7 @@
 export Server_Dir=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 
 # 加载.env变量文件
-source $Server_Dir/.env
+# source $Server_Dir/.env
 
 # 给二进制启动程序、脚本等添加可执行权限
 chmod +x $Server_Dir/bin/*
@@ -36,38 +36,38 @@ Secret=${CLASH_SECRET:-$(openssl rand -hex 32)}
 
 # 自定义action函数，实现通用action功能
 success() {
-	echo -en "\\033[60G[\\033[1;32m  OK  \\033[0;39m]\r"
-	return 0
+        echo -en "\\033[60G[\\033[1;32m  OK  \\033[0;39m]\r"
+        return 0
 }
 
 failure() {
-	local rc=$?
-	echo -en "\\033[60G[\\033[1;31mFAILED\\033[0;39m]\r"
-	[ -x /bin/plymouth ] && /bin/plymouth --details
-	return $rc
+        local rc=$?
+        echo -en "\\033[60G[\\033[1;31mFAILED\\033[0;39m]\r"
+        [ -x /bin/plymouth ] && /bin/plymouth --details
+        return $rc
 }
 
 action() {
-	local STRING rc
+        local STRING rc
 
-	STRING=$1
-	echo -n "$STRING "
-	shift
-	"$@" && success $"$STRING" || failure $"$STRING"
-	rc=$?
-	echo
-	return $rc
+        STRING=$1
+        echo -n "$STRING "
+        shift
+        "$@" && success $"$STRING" || failure $"$STRING"
+        rc=$?
+        echo
+        return $rc
 }
 
 # 判断命令是否正常执行 函数
 if_success() {
-	local ReturnStatus=$3
-	if [ $ReturnStatus -eq 0 ]; then
-		action "$1" /bin/true
-	else
-		action "$2" /bin/false
-		exit 1
-	fi
+        local ReturnStatus=$3
+        if [ $ReturnStatus -eq 0 ]; then
+                action "$1" /bin/true
+        else
+                action "$2" /bin/false
+                exit 1
+        fi
 }
 
 
@@ -80,8 +80,8 @@ source $Server_Dir/scripts/get_cpu_arch.sh
 
 # Check if we obtained CPU architecture
 if [[ -z "$CpuArch" ]]; then
-	echo "Failed to obtain CPU architecture"
-	exit 1
+        echo "Failed to obtain CPU architecture"
+        exit 1
 fi
 
 
@@ -113,17 +113,17 @@ Text4="配置文件config.yaml下载失败，退出启动！"
 curl -L -k -sS --retry 5 -m 10 -o $Temp_Dir/clash.yaml $URL
 ReturnStatus=$?
 if [ $ReturnStatus -ne 0 ]; then
-	# 如果使用curl下载失败，尝试使用wget进行下载
-	for i in {1..10}
-	do
-		wget -q --no-check-certificate -O $Temp_Dir/clash.yaml $URL
-		ReturnStatus=$?
-		if [ $ReturnStatus -eq 0 ]; then
-			break
-		else
-			continue
-		fi
-	done
+        # 如果使用curl下载失败，尝试使用wget进行下载
+        for i in {1..10}
+        do
+                wget -q --no-check-certificate -O $Temp_Dir/clash.yaml $URL
+                ReturnStatus=$?
+                if [ $ReturnStatus -eq 0 ]; then
+                        break
+                else
+                        continue
+                fi
+        done
 fi
 if_success $Text3 $Text4 $ReturnStatus
 
@@ -133,14 +133,14 @@ if_success $Text3 $Text4 $ReturnStatus
 
 ## 判断订阅内容是否符合clash配置文件标准，尝试转换（当前不支持对 x86_64 以外的CPU架构服务器进行clash配置文件检测和转换，此功能将在后续添加）
 if [[ $CpuArch =~ "x86_64" || $CpuArch =~ "amd64"  ]]; then
-	echo -e '\n判断订阅内容是否符合clash配置文件标准:'
-	bash $Server_Dir/scripts/clash_profile_conversion.sh
-	sleep 3
+        echo -e '\n判断订阅内容是否符合clash配置文件标准:'
+        bash $Server_Dir/scripts/clash_profile_conversion.sh
+        sleep 3
 fi
 
 
 ## Clash 配置文件重新格式化及配置
-# 取出代理相关配置 
+# 取出代理相关配置
 #sed -n '/^proxies:/,$p' $Temp_Dir/clash.yaml > $Temp_Dir/proxy.txt
 sed -n '/^proxies:/,$p' $Temp_Dir/clash_config.yaml > $Temp_Dir/proxy.txt
 
@@ -156,58 +156,10 @@ sed -ri "s@^# external-ui:.*@external-ui: ${Dashboard_Dir}@g" $Conf_Dir/config.y
 sed -r -i '/^secret: /s@(secret: ).*@\1'${Secret}'@g' $Conf_Dir/config.yaml
 
 
-## 启动Clash服务
-echo -e '\n正在启动Clash服务...'
-Text5="服务启动成功！"
-Text6="服务启动失败！"
-if [[ $CpuArch =~ "x86_64" || $CpuArch =~ "amd64"  ]]; then
-	nohup $Server_Dir/bin/clash-linux-amd64 -d $Conf_Dir &> $Log_Dir/clash.log &
-	ReturnStatus=$?
-	if_success $Text5 $Text6 $ReturnStatus
-elif [[ $CpuArch =~ "aarch64" ||  $CpuArch =~ "arm64" ]]; then
-	nohup $Server_Dir/bin/clash-linux-arm64 -d $Conf_Dir &> $Log_Dir/clash.log &
-	ReturnStatus=$?
-	if_success $Text5 $Text6 $ReturnStatus
-elif [[ $CpuArch =~ "armv7" ]]; then
-	nohup $Server_Dir/bin/clash-linux-armv7 -d $Conf_Dir &> $Log_Dir/clash.log &
-	ReturnStatus=$?
-	if_success $Text5 $Text6 $ReturnStatus
-else
-	echo -e "\033[31m\n[ERROR] Unsupported CPU Architecture！\033[0m"
-	exit 1
-fi
-
 # Output Dashboard access address and Secret
 echo ''
 echo -e "Clash Dashboard 访问地址: http://<ip>:9090/ui"
 echo -e "Secret: ${Secret}"
 echo ''
 
-# 添加环境变量(root权限)
-cat>/etc/profile.d/clash.sh<<EOF
-# 开启系统代理
-function proxy_on() {
-	export http_proxy=http://127.0.0.1:7890
-	export https_proxy=http://127.0.0.1:7890
-	export no_proxy=127.0.0.1,localhost
-    	export HTTP_PROXY=http://127.0.0.1:7890
-    	export HTTPS_PROXY=http://127.0.0.1:7890
- 	export NO_PROXY=127.0.0.1,localhost
-	echo -e "\033[32m[√] 已开启代理\033[0m"
-}
-
-# 关闭系统代理
-function proxy_off(){
-	unset http_proxy
-	unset https_proxy
-	unset no_proxy
-  	unset HTTP_PROXY
-	unset HTTPS_PROXY
-	unset NO_PROXY
-	echo -e "\033[31m[×] 已关闭代理\033[0m"
-}
-EOF
-
-echo -e "请执行以下命令加载环境变量: source /etc/profile.d/clash.sh\n"
-echo -e "请执行以下命令开启系统代理: proxy_on\n"
-echo -e "若要临时关闭系统代理，请执行: proxy_off\n"
+$Server_Dir/bin/clash-linux-amd64 -d $Conf_Dir
